4f209dced559900eababa3cd6605ecdf73592c2b
Mekala Abhi Teja <43229390+abhiteja69@users.noreply.github.com>
2020-08-20 12:49:33 +0530

    17th question

---
+++ sort.java
@@ -0,0 +1,52 @@
+//Q17 : Sorting a stack using a temporary stack
+
+
+
+
+
+import java.util.*;
+
+class sortstack
+{
+
+    public static Stack<Integer> sortstack(Stack<Integer>
+                                                   input)
+    {
+        Stack<Integer> tmpStack = new Stack<Integer>();
+        while(!input.isEmpty())
+        {
+            int tmp = input.pop();
+
+
+            while(!tmpStack.isEmpty() && tmpStack.peek()
+                    > tmp)
+            {
+
+                input.push(tmpStack.pop());
+            }
+
+            tmpStack.push(tmp);
+        }
+        return tmpStack;
+    }
+
+    public static void main(String args[])
+    {
+        Stack<Integer> input = new Stack<Integer>();
+        input.add(55);
+        input.add(41);
+        input.add(26);
+        input.add(89);
+        input.add(75);
+        input.add(32);
+
+
+        Stack<Integer> tmpStack=sortstack(input);
+        System.out.println("Sorted numbers are:");
+
+        while (!tmpStack.empty())
+        {
+            System.out.print(tmpStack.pop()+" ");
+        }
+    }
+}
